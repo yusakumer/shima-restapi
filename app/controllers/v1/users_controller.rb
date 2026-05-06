4879c32b8362bf04ad36db50fdd31c657b7cd039
yusaku <s.yusaku1220@gmail.com>
2026-05-07 07:20:27 +0900

    api作成

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    users = User.all
+    render json: users, status: :ok
+  end
+
+  def show
+    render json: @user, status: :ok
+  end
+
+  def create
+    User.create!(user_params)
+  end
+
+  def update
+    @user.update!(user_params)
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :nickname)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end

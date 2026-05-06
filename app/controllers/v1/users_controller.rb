d681121382ab0af0f200bb0a057fb19fd2e247cc
yusaku <s.yusaku1220@gmail.com>
2026-05-07 07:42:26 +0900

    statuscode 追加

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -12,10 +12,12 @@ class V1::UsersController < ApplicationController
 
   def create
     User.create!(user_params)
+    head :created
   end
 
   def update
     @user.update!(user_params)
+    head :no_content
   end
 
   def destroy
